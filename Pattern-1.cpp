6069c35406fa390efa1ed566e818593820830370
milichothani <milichothani4@gmail.com>
2026-06-30 12:46:50 +0530

    Updated Pattern-1.cpp

--- Pattern-1.cpp
+++ Pattern-1.cpp
@@ -1,13 +1,18 @@
 #include<iostream>
 using namespace std;
 
-int main(){
-    for(int i=0; i<4; i++){
-        for(int j=0; j<4; j++){
+void printPattern(int n){
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
             cout<<"*";
         }
         cout<<endl;
     }
+}
+int main(){
+    int n;
+    cin>>n;
+    printPattern(n);
     return 0;
 }
 
